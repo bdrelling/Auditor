10eb97014bc19c77541bbbbdc439f927f45920ea
Brian Drelling <brian@drelling.com>
2022-09-09 13:00:30 -0500

    Added messages to all errors

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Auditor
 
-[![CI Status](https://github.com/bdrelling/Auditor/actions/workflows/tests.yml/badge.svg)](https://github.com/bdrelling/Auditor/actions/workflows/audit.yml)
+[![CI Status](https://github.com/bdrelling/Auditor/actions/workflows/audit.yml/badge.svg)](https://github.com/bdrelling/Auditor/actions/workflows/audit.yml)
 [![License](https://img.shields.io/github/license/bdrelling/Auditor?label=)](https://github.com/bdrelling/Auditor/blob/main/LICENSE)
 
 **Auditor** is a tool used to ensure GitHub repositories across users and orgs are aligned and up to date with a common set of rules or expectations.
--- Tests/GitHubAuditTests/RepositoryValidating.swift
+++ Tests/GitHubAuditTests/RepositoryValidating.swift
@@ -23,62 +23,62 @@ extension RepositoryValidating {
         }
 
         // The default branch should be "main" for all repositories.
-        XCTAssertEqual(repository.defaultBranch, "main", repository.name)
+        XCTAssertEqual(repository.defaultBranch, "main", "Default branch is '\(repository.defaultBranch)', expected 'main'. (\(repository.name))")
 
         // Forking and Downloads should be enabled for all repositories.
-        XCTAssertEqual(repository.allowsForking, true, repository.name)
-        XCTAssertEqual(repository.hasDownloads, true, repository.name)
+        XCTAssertTrue(repository.allowsForking, "Forking should be enabled for all repositories. (\(repository.name))")
+        XCTAssertTrue(repository.hasDownloads, "Downloads should be enabled for all repositories. (\(repository.name))")
 
         // Pages, Projects, and Wiki should be disabled for all repositories.
-        XCTAssertEqual(repository.hasPages, false, repository.name)
-        XCTAssertEqual(repository.hasProjects, false, repository.name)
-        XCTAssertEqual(repository.hasWiki, false, repository.name)
+        XCTAssertFalse(repository.hasPages, "Pages should be disabled for all repositories. (\(repository.name))")
+        XCTAssertFalse(repository.hasProjects, "Projects should be disabled for all repositories. (\(repository.name))")
+        XCTAssertFalse(repository.hasWiki, "Wiki should be disabled for all repositories. (\(repository.name))")
 
         // None of my repositories should be disabled.
         // Honestly, not even sure what "disabled" implies here...
-        XCTAssertFalse(repository.isDisabled, repository.name)
+        XCTAssertFalse(repository.isDisabled, "Repository should not be disabled. (\(repository.name))")
 
         // All of my non-meta public repositories should have topics.
-        XCTAssertGreaterThan(repository.topics.count, 0, repository.name)
+        XCTAssertGreaterThan(repository.topics.count, 0, "No topics found. Repositories should have at least 1 topic. (\(repository.name))")
 
         // The remaining checks are only applicable to non-config, non-README repositories.
         guard repository.name != ".github", !repository.topics.contains("readme") else {
             // My README and config repositories should not have issues enabled.
-            XCTAssertEqual(repository.hasIssues, false, repository.name)
+            XCTAssertFalse(repository.hasIssues, "Issues should be disabled. (\(repository.name))")
 
             return
         }
 
         // All repositories should have a description and website.
-        XCTAssertTrue(repository.description?.isEmpty == false, repository.name)
-        XCTAssertTrue(repository.websiteURL?.isEmpty == false, repository.name)
+        XCTAssertTrue(repository.description?.isEmpty == false, "Description is missing. (\(repository.name))")
+        XCTAssertTrue(repository.websiteURL?.isEmpty == false, "Website URL is missing. (\(repository.name))")
 
         // The remaining checks are only applicable to non-archived repositories.
         guard !repository.isArchived else {
             // Archived repositories should NOT have issues enabled.
-            XCTAssertEqual(repository.hasIssues, false, repository.name)
+            XCTAssertFalse(repository.hasIssues, "Issues should be disabled. (\(repository.name))")
 
             return
         }
 
         // Web Commit Signoff should be required for all repositories.
-        XCTAssertEqual(repository.isWebCommitSignoffRequired, true, repository.name)
+        XCTAssertTrue(repository.isWebCommitSignoffRequired, "Web committ signoff should be required. (\(repository.name))")
 
         // None of my repositories should have any open issues -- they should all be addressed.
-        XCTAssertEqual(repository.numberOfOpenIssues, 0, repository.name)
+        XCTAssertEqual(repository.numberOfOpenIssues, 0, "\(repository.numberOfOpenIssues) issues need to be addressed. (\(repository.name))")
 
         // All of my repositories should have *at least* myself as a stargazer and watcher.
-        XCTAssertGreaterThan(repository.numberOfStargazers, 0, repository.name)
-        XCTAssertGreaterThan(repository.numberOfWatchers, 0, repository.name)
+        XCTAssertGreaterThan(repository.numberOfStargazers, 0, "No stargazers found. You didn't star your own repository! (\(repository.name))")
+        XCTAssertGreaterThan(repository.numberOfWatchers, 0, "No watchers found. You aren't watching your own repository! (\(repository.name))")
 
         if repository.owner.username == "swift-kipple" {
             // All non-README swift-kipple libraries should be in Swift.
-            XCTAssertEqual(repository.language, "Swift", repository.name)
+            XCTAssertEqual(repository.language, "Swift", "Primary language is '\(repository.language ?? "nil")', expected Swift. (\(repository.name))")
         }
 
         // Issues should be available for all unarchived, public repositories, except the "ci" and "Auditor" repositories.
         if repository.name != "ci", repository.name != "Auditor" {
-            XCTAssertEqual(repository.hasIssues, true, repository.name)
+            XCTAssertTrue(repository.hasIssues, "Issues should be enabled. (\(repository.name))")
         }
     }
 }
