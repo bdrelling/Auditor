ba78cb85bb2920d5a6b7a8ee3605e94804b4da0c
Brian Drelling <brian@drelling.com>
2023-03-29 13:55:09 -0500

    Updated Package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version: 5.7
 
 import PackageDescription
 
@@ -8,8 +8,7 @@ let package = Package(
         .macOS(.v12),
     ],
     dependencies: [
-        .package(url: "https://github.com/bdrelling/GoatHerb", from: "0.2.1"),
-        .package(url: "https://github.com/swift-kipple/Tools", from: "0.3.1"),
+        .package(url: "https://github.com/bdrelling/GoatHerb", from: "0.3.0"),
     ],
     targets: [
         .testTarget(
